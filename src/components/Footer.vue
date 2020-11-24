c098eaf759e51c05970fe7eb1411d3a960a42c43
lyndexer <mckinney.m93@gmail.com>
2020-11-23 16:13:50 -0800

    additional skeleton work on portfolio, cleaning out starter scaff

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer>
+    <h1>social stuff here pls</h1>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style></style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="nav">
+    <router-link to="/work">Work</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Nav"
+};
+</script>
+
+<style></style>
