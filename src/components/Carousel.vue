8a7173ed19b2c362b200ff3d7428bf67be0f064c
lyndexer <mckinney.m93@gmail.com>
2020-12-01 17:03:50 -0800

    carousel is working

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -8,13 +8,19 @@
       :icon-prev="iconPrev"
       :icon-next="iconNext"
       :icon-size="iconSize"
+      :indicator-inside="false"
+      :autoplay="false"
     >
       <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
-        <section :class="`hero is-large is-${carousel.color}`">
-          <div class="hero-body has-text-centered">
-            <h1 class="title">{{ carousel.title }}</h1>
+        <section class="hero is-large">
+          <div class="hero-body">
+            <img :src="`${carousel.gif}`" />
           </div>
         </section>
+        <div class="button-box">
+          <a class="button is-link" :href="`${carousel.demo}`">Demo</a>
+          <a class="button is-link" :href="`${carousel.source}`">Source Code</a>
+        </div>
       </b-carousel-item>
     </b-carousel>
   </section>
@@ -29,12 +35,51 @@ export default {
       arrowBoth: true,
       arrowHover: true,
       carousels: [
-        { title: "Slide 1", color: "info" },
-        { title: "Slide 2", color: "success" },
-        { title: "Slide 3", color: "warning" },
-        { title: "Slide 4", color: "danger" }
+        {
+          name: "PhotoTagApp",
+          gif: "https://i.imgur.com/oTzN7VY.gif",
+          demo: "https://where-s-waldo-cf908.web.app/#/",
+          source:
+            "https://github.com/MMathew93/Photo-Tagging-App/tree/master/photo-tagging-app"
+        },
+        {
+          name: "WeatherApp",
+          gif: "https://i.imgur.com/bmtieJt.gif",
+          demo: "#",
+          source: "https://github.com/MMathew93/Weather-App"
+        },
+        {
+          name: "BattleshipApp",
+          gif: "https://i.imgur.com/n3kdxUc.gif",
+          demo: "#",
+          source:
+            "https://github.com/MMathew93/Battle-Ship/tree/master/battle-ship"
+        },
+        {
+          name: "BlogUserApp",
+          gif: "https://i.imgur.com/ittvW8m.gif",
+          demo: "#",
+          source: "https://github.com/MMathew93/Blog-Project"
+        },
+        {
+          name: "Discordbot",
+          gif: "https://i.imgur.com/NwYJ8ID.gif",
+          demo: "#",
+          source: "https://github.com/MMathew93/heh-bot"
+        },
+        {
+          name: "MembersOnlyApp",
+          gif: "https://i.imgur.com/Y3ucToS.gif",
+          demo: "#",
+          source: "https://github.com/MMathew93/members-only"
+        }
       ]
     };
+  },
+  methods: {
+    getGif() {
+      return `/assets/work gifs/0.gif`;
+    }
   }
 };
 </script>
@@ -49,10 +94,11 @@ export default {
   margin: 0 20px;
 }
 
-/*Mobile Mix-ins*/
-@media screen and (max-width: 1024px) {
-  .hero {
-    height: 400px;
-  }
+.hero-body {
+  padding: 0 !important;
+}
+
+.is-link {
+  max-width: 200px;
 }
 </style>
